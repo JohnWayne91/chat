59cb832ce359ac3b473c5e6622b1d7182df29348
Vladislav <vladislav.sytyuk@gmail.com>
2021-10-01 17:50:02 +0300

    Add field 'delay' ro comment form

--- posts/mainapp/forms.py
+++ posts/mainapp/forms.py
@@ -2,21 +2,27 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+from bootstrap_datepicker_plus import DateTimePickerInput
+
 from .models import Comment
+
+
+class DateInput(forms.DateTimeInput):
+    input_type = 'date'
 
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ('text', 'delay',)
+        widgets = {'delay': DateTimePickerInput()}
 
 
 class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    # captcha = CaptchaField()
 
     class Meta:
         model = User
