164b6f0af5c3be73a841b345848718d1bdee712b
Vladislav <vladislav.sytyuk@gmail.com>
2021-10-04 13:50:17 +0300

    Make refactoring

--- posts/mainapp/consumers.py
+++ posts/mainapp/consumers.py
@@ -1,6 +1,5 @@
 import json
 import asyncio
-from pytz import timezone
 from datetime import datetime
 
 from channels.generic.websocket import AsyncWebsocketConsumer
--- posts/mainapp/views.py
+++ posts/mainapp/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import login, logout
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
