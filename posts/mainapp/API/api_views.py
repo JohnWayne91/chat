c0f8838413271d5f05613d72310e8059eff02d97
Vladislav <vladislav.sytyuk@gmail.com>
2021-10-11 16:02:01 +0300

    Update the api due to changes in models

--- posts/mainapp/API/api_views.py
+++ posts/mainapp/API/api_views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView, CreateAPIView
 from rest_framework.filters import SearchFilter
 
-from .serializers import PostSerializer
-from ..models import Posts
+from .serializers import PostSerializer, CommentSerializer
+from ..models import Posts, Comment
 
 
 class PostListApiView(ListAPIView, ListCreateAPIView):
@@ -16,3 +16,11 @@ class PostDetailApiView(RetrieveAPIView, RetrieveUpdateAPIView, CreateAPIView):
     serializer_class = PostSerializer
     queryset = Posts.objects.all()
     lookup_field = 'id'
+
+
+class CommentsListApiView(ListAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    # lookup_field = 'related_post'
+    filter_backends = [SearchFilter]
+    search_fields = ['related_post__title']
--- posts/mainapp/API/urls.py
+++ posts/mainapp/API/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .api_views import PostListApiView, PostDetailApiView
+from .api_views import PostListApiView, PostDetailApiView, CommentsListApiView
 
 
 urlpatterns = [
     path('posts/', PostListApiView.as_view(), name='posts_list'),
-    path('post/<str:id>/', PostDetailApiView.as_view(), name='post_detail')
+    path('post/<str:id>/', PostDetailApiView.as_view(), name='post_detail'),
+    path('comments/', CommentsListApiView.as_view(), name='comments_list')
 ]
